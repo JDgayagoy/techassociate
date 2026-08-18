5c699ff7425d80f434c3e0239d44a2b8be1c3fea
JDgayagoy <jdgprofessional@gmail.com>
2026-08-18 14:01:13 +0800

    feat(backend): add task controller and wire up app module

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { TaskModel } from 'generated/prisma/models';
+import { Prisma,Task } from 'generated/prisma/browser';
+
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly taskService: TaskService
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('task')
+  async getTask(@Query('status') status?: string){
+    return this.taskService.getAll(status);
+  }
+
+  @Post('task')
+  async createTask(
+    @Body() taskData: { title: string; description?: string }
+  ) {
+    return this.taskService.createTask(taskData);
+  }
+
+  @Patch('task/:id')
+  async updateTask(
+    @Param('id') id: string,
+    @Body() data: Prisma.TaskUpdateInput,
+  ) {
+    return this.taskService.updateTask({
+      where: {
+        id: Number(id),
+      },
+      data,
+    });
+  }
+
+  @Delete('task/:id')
+  async deleteTask(@Param('id') id: string) {
+    return this.taskService.deleteTask({
+      id: Number(id),
+    });
   }
 }
